914742d8303cb51f70a1e6f3b5e1307812aa10c6
chensj <JohnnyCsj666@gmail.com>
2025-08-05 15:09:40 +0800

    update setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Python tools for eROSITA data analysis.',
     author='Shi-Jiang Chen',
     author_email='JohnnyCsj666@gmail.com',
-    #url='https://github.com/AstroChensj/Xstack.git',
+    url='https://github.com/AstroChensj/erotools.git',
     packages=['erotools','erotools_scripts','examples'],
     install_requires=[
         'astropy',
@@ -21,8 +21,11 @@ setup(
     package_data={'erotools': ["*.fits.gz",'*.fits']},
     entry_points={
         'console_scripts': [
-            'run_apetool = erotools_scripts.apetool_autoscript:main',
-            'get_ero_skytile = erotools_scripts.get_ero_skytile:main'
+            'erosrcdet = erotools_scripts.srcdet:main',
+            'eroforcedphot = erotools_scripts.forcedphot:main',
+            'eroforcedspec = erotools_scripts.forcedspec:main',
+            'erotile_api = erotools_scripts.gettile_api:main',
+            'erotile_local = erotools_scripts.gettile_local:main',
         ]
     }
 )
